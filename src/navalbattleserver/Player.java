533afc12331e91d90dc222e74753931d042f800f
Pietro Bonechi <p.bonechi1@gmail.com>
2019-11-02 11:18:41 +0100

    add class and project

---
+++ src/navalbattleserver/Player.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package navalbattleserver;
+
+/**
+ *
+ * @author pbone
+ */
+public class Player {
+    public int iId;
+    public String sName;
+
+    public Player(int iId, String sName) {
+        this.iId = iId;
+        this.sName = sName;
+    }
+    
+}
