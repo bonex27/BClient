758efffb4fd8c9b1cb328ad01065675bca54d8e8
lai2 <informatica@lai2>
2019-11-06 11:12:45 +0100

    Socket conection

--- src/bclient/BClient.java
+++ src/bclient/BClient.java
@@ -5,8 +5,18 @@
  */
 package bclient;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
+import javax.swing.*;
+
+
 
 /**
  *
@@ -18,12 +28,57 @@ public class BClient {
      * @param args the command line arguments
      * @throws java.io.IOException
      */
-    public static void main(String[] args) throws IOException{
-        if (args.length != 1) {
-            System.err.println("Pass the server IP as the sole command line argument");
-            return;
+    
+    private JFrame frame = new JFrame("Naval Battle");
+    private JLabel messageLabel = new JLabel("...");
+
+    private Square[] board = new Square[9];
+    private Square currentSquare;
+
+    private Socket socket;
+    private Scanner in;
+    private PrintWriter out;
+    
+    public BClient(String serverAddress) throws IOException{
+        
+        socket = new Socket(serverAddress, 6012);
+        in = new Scanner(socket.getInputStream());
+        out = new PrintWriter(socket.getOutputStream(), true);
+        
+        /*messageLabel.setBackground(Color.lightGray);
+        frame.getContentPane().add(messageLabel, BorderLayout.SOUTH);
+        
+        JPanel boardPanel = new JPanel();
+        boardPanel.setBackground(Color.black);
+        boardPanel.setLayout(new GridLayout(21, 21, 2, 2));
+        
+        for (int i = 0; i < board.length; i++) {
+            final int j = i;
+            board[i] = new Square();
+            board[i].addMouseListener(new MouseAdapter() {
+                public void mousePressed(MouseEvent e) {
+                    currentSquare = board[j];
+                    out.println("MOVE " + j);
+                }
+            });
+            boardPanel.add(board[i]);
         }
-        Socket socket= new Socket(args[0], 6012);
+        frame.getContentPane().add(boardPanel, BorderLayout.CENTER);*/
+    }
+    
+    public static void main(String[] args) throws IOException{
+        
+        BClient client = new BClient("172.16.10.72");//ip bonechi lai2-04
+        
+        /*try (Socket socket = new Socket("172.16.10.72", 6012)) {
+            Scanner scanner = new Scanner(System.in);
+            Scanner in = new Scanner(socket.getInputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            while (scanner.hasNextLine()) {
+                out.println(scanner.nextLine());
+                System.out.println(in.nextLine());
+            }
+        }*/
     }
     
 }
