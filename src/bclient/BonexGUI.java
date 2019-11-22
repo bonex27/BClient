c10ca5382bcf53fe3233fe6101d0b63e3a158918
pie <pie@192.168.43.192>
2019-11-22 18:42:30 +0100

    commit for adi

--- src/bclient/BonexGUI.java
+++ src/bclient/BonexGUI.java
@@ -5,6 +5,8 @@
  */
 package bclient;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -14,7 +16,7 @@ import java.util.Scanner;
  *
  * @author pbone
  */
-public class BonexGUI extends javax.swing.JFrame {
+public class BonexGUI extends javax.swing.JFrame implements KeyListener {
 
     String mess = "";
     
@@ -28,148 +30,22 @@ public class BonexGUI extends javax.swing.JFrame {
      */
     public BonexGUI()  {
         initComponents();
-        this.txtOutput.append("Inserisci l'indirizzo ip o il dominio del server\n");       
+        lblInput.addKeyListener(this);
+        this.txtOutput.append("Inserisci l'indirizzo ip o il dominio del server\n");
     }
     public void connect(String a)
     {
         try{
             socket = new Socket(this.lblInput.getText() , 6012);          
             this.txtOutput.append("Connesso al server di gioco\n");
-            this.run();
+          
         }   
         catch (IOException ex) {
             this.txtOutput.setText("Errore connessione" + ex);
         }
     }   
-    public void place(){
-        log = in.nextLine();
-        ArrOfStr = log.split("@",2);
-        
-        System.out.println("La barca "+ArrOfStr[1]+ " da posizionare è lunga "+ ArrOfStr[0] );
-        
-        System.out.println("Inserisci la x(0-20):");
-        log = scanner.nextLine()+"@";
-        System.out.println("Inserisci la y(0-20):");
-        log += scanner.nextLine();
-        
-        
-        System.out.println("In verticale(v) o in orizzontale(o):");
-        log += "@" + scanner.nextLine();
-             
-        out.println(log);
-    }
+ 
     
-    
-    private void attack(){
-        System.out.println("Inserisci le cordinate per l'attacco");
-        
-        System.out.println("Inserisci la x(0-20):");
-        log = scanner.nextLine()+"@";
-        System.out.println("Inserisci la y(0-20):");
-        log += scanner.nextLine();
-        
-        out.println(log);
-        log="";//reset dopo invio
-    }
-    
-    public void run() throws IOException{
-        in = new Scanner(socket.getInputStream());
-        out = new PrintWriter(socket.getOutputStream(), true);
-        do{
-        
-            try{
-                log=in.nextLine();
-                ArrOfStr=log.split("@",2);
-                this.lblPlayer.setText("Player " + ArrOfStr[0]);
-                this.txtOutput.append("Sei il giocatore " + ArrOfStr[0]+"\n");
-                this.txtOutput.revalidate();
-                switch(ArrOfStr[1]){
-                    case "p":                               //Place
-                        do{
-                            place();
-                            log=in.nextLine();
-                            if(log.equals("NEAR"))
-                                System.out.println("Dati inseriti non accettabili, reinserire");
-                        }while(log.equals("NEAR"));
-
-                        System.out.println("Barca posizionata correttamente");
-
-                        break;
-
-                    case "w":                               //wait
-                        System.out.println("Turno dell'avversario");
-                        break;
-
-                    case "a":                               //attack
-                        do{
-                            attack();
-                            log=in.nextLine();
-
-                            switch (log) {
-                                case "c":
-                                    System.out.println("Barca colpita");
-                                    break;
-                                case "d":
-                                    System.out.println("Barca distrutta");
-                                    break;
-                                case "m":
-                                    System.out.println("Barca mancata");
-                                    break;
-                                case "gc":
-                                    System.out.println("Barca già colpita");
-                                    break;
-                                case "f":
-                                    System.out.println("Dati inseriti non accettabili, reinserire");
-                                    break;
-                                case "win":
-                                    System.out.println("Hai vinto");
-                                    this.socket.close();
-                                    break;
-                            }
-
-                        }while(log.equals("f"));
-                        
-                        break;
-
-                    case "v":
-                        visual();
-                        break;
-                                                  
-                    case "lose":
-                        System.out.println("Hai perso");
-                        break;
-                }
-            }
-            catch(Exception e){
-                System.out.println("err" + e);
-                break;
-            }
-        }while(!ArrOfStr[1].equals("lose"));
-        
-        System.out.println("Gioco finito");
-        this.socket.close();
-    }
-    
-    public void visual(){
-        out.println("stampa");
-        System.out.println(in.nextLine().replaceAll("#", "\n"));
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    /**
-     * This method is called from within the constructor to initialize the form.
-     * WARNING: Do NOT modify this code. The content of this method is always
-     * regenerated by the Form Editor.
-     */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -180,6 +56,7 @@ public class BonexGUI extends javax.swing.JFrame {
         jScrollPane3 = new javax.swing.JScrollPane();
         txtOutput = new javax.swing.JTextArea();
         jLabel3 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -210,28 +87,33 @@ public class BonexGUI extends javax.swing.JFrame {
         jLabel3.setFont(new java.awt.Font("Lucida Grande", 0, 24)); // NOI18N
         jLabel3.setText("Battaglia navale");
 
+        jButton1.setText("jButton1");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addGap(185, 185, 185)
+                .addComponent(btnOk)
+                .addGap(102, 102, 102)
+                .addComponent(jButton1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jScrollPane3, javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(lblInput)))
+                        .addComponent(lblInput))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(16, 16, 16)
                         .addComponent(lblPlayer)
                         .addGap(50, 50, 50)
                         .addComponent(jLabel3)
-                        .addGap(0, 120, Short.MAX_VALUE)))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane3)))
                 .addContainerGap())
-            .addGroup(layout.createSequentialGroup()
-                .addGap(178, 178, 178)
-                .addComponent(btnOk)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -240,14 +122,15 @@ public class BonexGUI extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(lblPlayer)
                         .addGap(46, 46, 46))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)))
+                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(lblInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(btnOk)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnOk)
+                    .addComponent(jButton1))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -255,7 +138,7 @@ public class BonexGUI extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnOkActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOkActionPerformed
-        this.connect(this.lblInput.getText()); 
+
     }//GEN-LAST:event_btnOkActionPerformed
 
     private void lblInputActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_lblInputActionPerformed
@@ -300,10 +183,25 @@ public class BonexGUI extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnOk;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JTextField lblInput;
     private javax.swing.JLabel lblPlayer;
     private javax.swing.JTextArea txtOutput;
     // End of variables declaration//GEN-END:variables
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
